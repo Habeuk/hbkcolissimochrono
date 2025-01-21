688c023a7c9f3301d41bf9ad22a93124da5dd731
kouwa stephane <kksasteph888@gmail.com>
2025-01-21 12:19:11 +0100

    Maj

--- src/Plugin/Commerce/Condition/ShippingFilterByPayment.php
+++ src/Plugin/Commerce/Condition/ShippingFilterByPayment.php
@@ -9,8 +9,10 @@ use Drupal\physical\MeasurementType;
 use Drupal\physical\Weight;
 
 /**
- * Provides the weight condition for shipments.
+ * Provide the weight condition for shipments.
  *
+ * @deprecated car la livraison doit venir avant le paiement. ( Juste question
+ *             de bon sens et ces ce qui est fait partout ailleurs ).
  * @CommerceCondition(
  *   id = "hbk_shipping_filterby_payment",
  *   label = @Translation("show if method if Collsimo"),
@@ -30,24 +32,25 @@ class ShippingFilterByPayment extends ConditionBase {
     ] + parent::defaultConfiguration();
   }
   
-  /**
-   *
-   * {@inheritdoc}
-   */
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildConfigurationForm($form, $form_state);
-    $form['payment_gateways'] = [
-      '#type' => 'commerce_entity_select',
-      '#title' => $this->t('Payment gateways'),
-      '#default_value' => $this->configuration['payment_gateways'],
-      '#target_type' => 'commerce_payment_gateway',
-      '#hide_single_entity' => FALSE,
-      '#multiple' => TRUE,
-      '#required' => TRUE
-    ];
-    
-    return $form;
-  }
+  // /**
+  // *
+  // * {@inheritdoc}
+  // */
+  // public function buildConfigurationForm(array $form, FormStateInterface
+  // $form_state) {
+  // $form = parent::buildConfigurationForm($form, $form_state);
+  // $form['payment_gateways'] = [
+  // '#type' => 'commerce_entity_select',
+  // '#title' => $this->t('Payment gateways'),
+  // '#default_value' => $this->configuration['payment_gateways'],
+  // '#target_type' => 'commerce_payment_gateway',
+  // '#hide_single_entity' => FALSE,
+  // '#multiple' => TRUE,
+  // '#required' => false
+  // ];
+  
+  // return $form;
+  // }
   
   /**
    *
@@ -55,7 +58,6 @@ class ShippingFilterByPayment extends ConditionBase {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
-    
     $values = $form_state->getValue($form['#parents']);
     $this->configuration['payment_gateways'] = $values['payment_gateways'];
   }
@@ -65,26 +67,32 @@ class ShippingFilterByPayment extends ConditionBase {
    * {@inheritdoc}
    */
   public function evaluate(EntityInterface $entity) {
-    $this->assertEntity($entity);
+    return true;
+    // $this->assertEntity($entity);
     
-    /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
-    $order = $entity;
+    // /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
+    // $order = $entity;
     
-    if ($order->get('payment_gateway')->isEmpty()) {
-      // The payment gateway is not known yet, the condition cannot pass.
-      return FALSE;
-    }
-    // Avoiding ->target_id to allow the condition to be unit tested,
-    // because Prophecy doesn't support magic properties.
-    $payment_gateway_item = $order->get('payment_gateway')->first()->getValue();
-    $payment_gateway_id = $payment_gateway_item['target_id'];
-    // On a un bug voir la tache #831. Pour reduire l'impact du bug, on va
-    // valider la methode de livraisons si le champs 'payment_gateway' est vide.
-    // Pour que cela fonctionne bien il faudra que le premier moyen de paiement
-    // accepte tous les moyens de livraisons. ( exemple : on ne doit pas mettre
-    // paiement à la livraison en premier ).
-    if (!$payment_gateway_id)
-      return true;
-    return in_array($payment_gateway_id, $this->configuration['payment_gateways']);
+    // if ($order->get('payment_gateway')->isEmpty()) {
+    // // The payment gateway is not known yet, the condition cannot pass.
+    // return FALSE;
+    // }
+    // // Avoiding ->target_id to allow the condition to be unit tested,
+    // // because Prophecy doesn't support magic properties.
+    // $payment_gateway_item =
+    // $order->get('payment_gateway')->first()->getValue();
+    // $payment_gateway_id = $payment_gateway_item['target_id'];
+    // // On a un bug voir la tache #831. Pour reduire l'impact du bug, on va
+    // // valider la methode de livraisons si le champs 'payment_gateway' est
+    // vide.
+    // // Pour que cela fonctionne bien il faudra que le premier moyen de
+    // paiement
+    // // accepte tous les moyens de livraisons. ( exemple : on ne doit pas
+    // mettre
+    // // paiement à la livraison en premier ).
+    // if (!$payment_gateway_id)
+    // return true;
+    // return in_array($payment_gateway_id,
+    // $this->configuration['payment_gateways']);
   }
 }
